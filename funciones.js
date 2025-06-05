94a8d16d19878451083bfda2ce7cd4198e910d54
DeanMcAndrews <cesarandres.deanda@gmail.com>
2025-06-05 10:36:00 -0600

    2025/06/05 - Se agrega funciones nuevas

--- funciones.js
+++ funciones.js
@@ -1,3 +1,6 @@
-function hazAlgo() {
-  alert("Estoy haciendo algo!!");
+function sumarNumeros() {
+  let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+  let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+  let resultado = numero1 + numero2;
+  alert("El resultado de la suma es: " + resultado);
 }
