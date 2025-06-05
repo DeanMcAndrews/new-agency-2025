0de0f3c8c99afc0d4f286e2bc530ed64f4efd23f
Cesar Andres De Anda Alcantara <cesarandres.deanda@gmail.com>
2025-06-05 11:34:41 -0600

    2025/06/05 - Update funciones.js
    Se aprueba sugerencia. Gracias
    
    Co-authored-by: DeanMcAndrews1983 <dean.mcandrews1983@gmail.com>

--- funciones.js
+++ funciones.js
@@ -2,5 +2,5 @@ function sumarNumeros() {
   let numero1 = parseFloat(prompt("Ingrese el primer número:"));
   let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
   let resultado = numero1 + numero2;
-  alert("El resultado de la suma es: " + resultado);
+  alert("Resultado de la Suma: " + resultado);
 }
